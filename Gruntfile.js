910c695d732a04cf02f39cc24f3186ed83507416
Pedro Capriles <zearkiatos@Pedros-MacBook-Air.local>
2020-12-20 14:53:18 -0300

    Directives and fix compatibilities problem with angular and node version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,28 @@
 module.exports = function (grunt) {
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     connect: {
       server: {
         options: {
           port: 9000,
           base: 'public/'
         }
+      }
+    },
+    packageModules: {
+      dist: {
+        src: 'package.json',
+        dest: 'public/.tmp'
+      },
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
       }
     },
     watch: {
@@ -21,7 +37,9 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-package-modules');
 
-  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.registerTask('default', ['packageModules', 'connect', 'watch']);
 
 };
