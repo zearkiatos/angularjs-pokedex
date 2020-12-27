f0b5816e0a61511089ee32b87d395ddd8fa7eebb
Pedro Capriles <zearkiatos@Pedros-MacBook-Air.local>
2020-12-27 11:46:29 -0300

    Customizes directives

--- src/app.js
+++ src/app.js
@@ -1,48 +1,67 @@
-(function(){
- const app = angular.module('pokedex', []);
- app.controller('PokemonController', function() {
-     this.pokemon = {
-         id: '001',
-         name: 'Bulbasaur',
-         species: 'Seed Pokemon',
-         type: ['Grass', 'Poison'],
-         height: "2'4",
-         weight: '15.2 lbs',
-         abilities: ['Overgrow', 'Chlorophyll'],
-         stats: {
-             hp: 45,
-             attack: 49,
-             defense: 49,
-             "sp.atk": 65,
-             "sp.def": 65,
-             speed: 45,
-             total: 318
-         },
-         evolutions: ["Bulbasaur", "Ivysaur", "Venusaur"]
-     };
- });
+(function () {
+    const app = angular.module('pokedex', []);
+    app.controller('PokemonController', function () {
+        this.pokemon = {
+            id: '001',
+            name: 'Bulbasaur',
+            species: 'Seed Pokemon',
+            type: ['Grass', 'Poison'],
+            height: "2'4",
+            weight: '15.2 lbs',
+            abilities: ['Overgrow', 'Chlorophyll'],
+            stats: {
+                hp: 45,
+                attack: 49,
+                defense: 49,
+                "sp.atk": 65,
+                "sp.def": 65,
+                speed: 45,
+                total: 318
+            },
+            evolutions: ["Bulbasaur", "Ivysaur", "Venusaur"]
+        };
+    });
 
- app.controller('TabsController', function() {
-    this.tab = 1;
+    app.controller('TabsController', function () {
+        this.tab = 1;
 
-    this.selectTab = function(tab) {
-        this.tab = tab;
-    };
- });
+        this.selectTab = function (tab) {
+            this.tab = tab;
+        };
+    });
 
- app.controller('CommentController', function() {
-    this.comments = [];
-    this.show = false;
+    app.controller('CommentController', function () {
+        this.comments = [];
+        this.comment = {
+            anonymous: false,
+            email: "",
+            body: "",
+            date: Date.now()
+        };
+        this.show = false;
 
-    this.toggle = function() {
-        this.show = !this.show;
-    }
- });
+        this.toggle = function () {
+            this.show = !this.show;
+        };
 
- app.filter('imageify', function() {
-    return function(input, ext) {
-        const url = `assets/img/pokemons/${input.toLowerCase()}.${ext || 'jpg'}`;
-        return url;   
-    };
- });
+        this.anonymousChanged = function () {
+            if (this.comment.anonymous) {
+                this.comment.email = "";
+            }
+        };
+
+        this.addComment = function() {
+            this.comment.date = Date.now();
+            this.comments.push(this.comment);
+            this.comment = {};
+        };
+    });
+
+    app.filter('imageify', function () {
+        return function (input, ext) {
+            const url = `assets/img/pokemons/${input.toLowerCase()}.${ext || 'jpg'}`;
+            return url;
+        };
+    });
+
 })();
